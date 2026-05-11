26024f3d5dc0b8b60ad1c1a54635fae2c9d7aad6
Rachel <mango.loco.mama.coco@gmail.com>
2026-05-11 14:05:19 +0800

    Remove city parameter from all frontend calls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+echo "import { redirect } from 'next/navigation';
 
 export default function RootPage() {
-  redirect("/welcome");
-}
+  redirect('/welcome');
+}" > src/app/page.tsx
